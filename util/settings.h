d0a3c8ca23185df9923f06c2a29c5ec501207151
= <i.am@nilq.dk>
2016-10-18 23:49:43 +0200

    Add settings

---
+++ util/settings.h
@@ -0,0 +1,43 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+namespace settings {
+
+    const int WIDTH  = 1400;
+    const int HEIGHT = 800;
+
+    const int CZ = 50;
+
+    const int NUM_BOTS = 30;
+
+    const float BOT_RADIUS = 10;
+    const float BOT_SPEED  = 0.1;
+
+    const float SPIKE_SPEED = 0.005;
+    const float SPIKE_MULT  = 0.5;
+
+    const float BOOST_SIZE_MULT = 2;
+
+    const float REP_RATE = 7;
+
+    const float DIST = 150;
+    const float EYE_SENS = 2;
+    const float BLOOD_SENS = 0.002;
+
+    const float META_MUTRATE1 = 0.002;
+    const float META_MUTRATE2 = 0.05;
+
+    const float FOOD_GROWTH = 0;
+
+    const float FOOD_INTAKE   = 0.00325;
+    const float FOOD_WASTE    = 0.001;
+    const float FOOD_MAX      = 0.5;
+    const float FOOD_ADD_FREQ = 30;
+
+    const float FOOD_TRANSFER = 0.001;
+    const float FOOD_SHARING_DISTANC = 50;
+
+    const float FOOD_DISTRIBUTION_RADIUS = 100;
+}
+
+#endif
