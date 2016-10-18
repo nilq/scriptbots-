f4cf705dc88c07ec3e58bef77a2b572c313e23c4
= <i.am@nilq.dk>
2016-10-18 21:14:23 +0200

    Add math

---
+++ math.cpp
@@ -0,0 +1,14 @@
+#include "math.h"
+
+#ifdef
+
+#ifdef MATH_NAMESPACE
+namespace MATH_NAMESPACE {
+#endif
+
+    template class Vector2<float>;
+    template class Vector2<double>;
+
+#ifdef MATH_NAMESPACE
+}
+#endif
